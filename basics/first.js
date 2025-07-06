1a16ab8f58f4af756c613317d94dd84323fff8a3
Chopda Armik <armikchopda35280@gmail.com>
2025-07-06 11:04:04 +0000

    module

--- basics/first.js
+++ basics/first.js
@@ -1,1 +1,4 @@
-console.log('hello  armik');
+const math=require('./math')
+console.log(math.add(2,3));
+console.log(math.sub(2,3));
+// console.log(math.multi(2,3));
---
+++ basics/math.js
@@ -0,0 +1,13 @@
+function add(a,b){
+    return a+b
+}
+function  sub(a,b){
+    return a-b
+}
+// exports.div=(a,b)=>a/b;   //anther way
+// exports.multi=(a,b)=>a*b;
+module.exports = {
+    add,sub
+}
+
+
