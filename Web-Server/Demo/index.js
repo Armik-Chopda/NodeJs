85cc982f479ea71b766d6179c35ca8209909c7b8
Chopda Armik <armikchopda35280@gmail.com>
2025-07-09 02:29:02 +0000

    http method

--- Web-Server/Demo/index.js
+++ Web-Server/Demo/index.js
@@ -3,22 +3,46 @@ const file=require("fs")
 const url=require("url")
 const myServer=http.createServer((req,res)=>{
     const myUrl=url.parse(req.url,true)
-    const log = `date= ${Date.now()}\n`;
+    const log = `date= ${Date.now()} methord= ${req.method}\n`;
     file.appendFile("./demo.txt",log,(err)=>{
+       
         if(err){
             console.log(err);
             
         }
         else{
+         
             console.log(myUrl);
             
-            res.end("wallcome bose")
+          
+            switch(myUrl.pathname){
+                case '/a':
+                    if(req.method==="GET"){
+                        console.log("wellcome to home page");
+                         res.end("Welcome to home page");
+                        
+                    }
+                    break;
+                case '/about':
+                    res.end("I Am Armik!");
+                    break;
+                case '/login':
+                    res.end("login page");
+                    
+                    if(req.method==="POST"){
+                        res.end("success!")
+                    }  
+                    break; 
+                default:
+                    res.end("Hello from Armik!");
+                    break;    
+            }
         }
 
     });
     
 
 });
-myServer.listen(9000, () => {
+myServer.listen(7000, () => {
     console.log("Server started on port 9000");
 });
